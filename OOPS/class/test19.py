13551ce327e5d30c19d420e47b030c75f8c1ad1f
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2025-01-23 19:15:22 -0500

    23 jan

---
+++ OOPS/class/test19.py
@@ -0,0 +1,14 @@
+class student19:
+    school='durga'   # class variable
+
+    def __init__(self,name,rollno):
+        self.name=name  #instance variable
+        self.rollno=rollno
+
+    def getinfo(self):    #instance method
+        print('my name is :',self.name)
+        print('my rollno is :', self.rollno)
+
+    @classmethod   #class method
+    def getschoolinfo(cls):
+        print('my school name is :',cls.school)
