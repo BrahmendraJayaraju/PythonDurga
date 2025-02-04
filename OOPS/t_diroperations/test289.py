eae5a376cf22d1502b9eda43550519d2bbff6b81
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2025-02-04 14:45:03 -0500

    23 jan

---
+++ OOPS/t_diroperations/test289.py
@@ -0,0 +1,7 @@
+#to get current working directory location
+
+import os
+cwd=os.getcwd()
+print('this is current dir',cwd)
+
+
---
+++ OOPS/t_diroperations/test301.py
@@ -0,0 +1,9 @@
+# to open notes 
+
+import os
+
+# List files and directories
+os.system('ls')
+
+# Open Notes app
+os.system('open -a "Notes"')
