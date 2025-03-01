1b50495abb5266c757c65502c403f5f14e4d4524
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2025-03-01 01:51:50 -0500

    23 jan

---
+++ akshay/diamond.py
@@ -0,0 +1,23 @@
+def pattern(n):
+
+    for j in range(1,n+1):
+        k = j
+        for i in range(1,n+1):
+            if i>j and i+j>n+1:
+                print(k,end=' ')
+            else:
+                print(' ',end=' ')
+
+        k=j
+        for i in range(1,n+1):
+            if i<=j and i+j<=n+1:
+                print(k,end=' ')
+            else:
+                print(' ',end=' ')
+
+
+
+        print()
+
+
+pattern(9)
